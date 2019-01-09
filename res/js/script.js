c44fdea3d762fa346f3fd1e3fa95e53fe9279513
Stephen <schen288@uottawa.ca>
2019-01-09 03:25:12 -0500

    touched up the website, added javascript animations

--- res/js/script.js
+++ res/js/script.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
     
-    
-    
-    
+    /* Sticky Navigation */
+    $('.js--section-about').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        }  
+        else{
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px'
+    });
     
     /* Navigation Scroll */
     $('a[href*="#"]')
@@ -41,5 +49,47 @@ $(document).ready(function() {
         }
       });
     
+    /* Animations on Scroll */
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });    
+    
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });      
+    
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });    
+    
+    $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated flipInX');
+    }, {
+        offset: '50%'
+    });
+    
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('fa-bars')){
+            icon.addClass('fa-times');
+            icon.removeClass('fa-bars');
+        }
+        else{
+            icon.addClass('fa-bars');
+            icon.removeClass('fa-times');
+        }
+    });    
+
     
 });
